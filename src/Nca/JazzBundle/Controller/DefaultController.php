974a8bbebd662779c51693befa6b89e4ae6f31db
Olivier Revollat <revollat@gmail.com>
2013-02-26 23:47:08 +0100

    commit :)

--- src/Nca/JazzBundle/Controller/DefaultController.php
+++ src/Nca/JazzBundle/Controller/DefaultController.php
@@ -2,7 +2,9 @@
 
 namespace Nca\JazzBundle\Controller;
 
+use Nca\JazzBundle\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -10,4 +12,38 @@ class DefaultController extends Controller
     {
         return $this->render('NcaJazzBundle:Default:index.html.twig', array('name' => $name));
     }
+    
+    public function createAction()
+    {
+        $product = new Product();
+        $product->setName('A Foo Bar héhé');
+        $product->setPrice('19.99');
+        $product->setDescription('Lorem ipsum dolor');
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $em->persist($product);
+        $em->flush();
+
+        return new Response('Produit créé avec id '.$product->getId());
+    }
+    
+    public function showAction($id)
+    {
+        $product = $this->getDoctrine()
+            ->getRepository('NcaJazzBundle:Product')
+            ->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException('Produit non trouvé avec id '.$id);
+        }
+
+        return $this->render('NcaJazzBundle:Product:product.html.twig', array('product' => $product));
+    }
+    
+    public function showAllAction(){
+        $em = $this->getDoctrine()->getEntityManager();
+        $products = $em->getRepository('NcaJazzBundle:Product')->findAllOrderedByName();
+        return $this->render('NcaJazzBundle:Product:products.html.twig', array('products' => $products));
+    }
+
 }
---
+++ src/Nca/JazzBundle/Entity/Category.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Nca\JazzBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Category
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Category
+{
+    
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+    }
+    
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Product", mappedBy="category")
+     */
+    protected $products;
+    
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Category
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Add products
+     *
+     * @param \Nca\JazzBundle\Entity\Product $products
+     * @return Category
+     */
+    public function addProduct(\Nca\JazzBundle\Entity\Product $products)
+    {
+        $this->products[] = $products;
+    
+        return $this;
+    }
+
+    /**
+     * Remove products
+     *
+     * @param \Nca\JazzBundle\Entity\Product $products
+     */
+    public function removeProduct(\Nca\JazzBundle\Entity\Product $products)
+    {
+        $this->products->removeElement($products);
+    }
+
+    /**
+     * Get products
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getProducts()
+    {
+        return $this->products;
+    }
+}
